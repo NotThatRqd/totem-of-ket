9143a3c505cb20e27cd917d5c5740b56961b5bca
NotThatRqd <67846317+NotThatRqd@users.noreply.github.com>
2023-02-07 17:40:43 -0800

    add save_save_file()

--- src/file_loader.rs
+++ src/file_loader.rs
@@ -41,33 +41,38 @@ impl Default for PlayerData {
 }
 
 #[derive(Debug)]
-pub enum LoadSaveFileError {
+pub enum SaveFileError {
     IoError(std::io::Error),
     JsonError(serde_json::error::Error),
 }
 
 // returns a result of either a PlayerData struct or a file not found error
-pub fn load_save_file(path: &str) -> Result<PlayerData, LoadSaveFileError>{
+pub fn load_save_file(path: &str) -> Result<PlayerData, SaveFileError>{
 
     let file_contents = match fs::read_to_string(path) {
         Ok(file_contents) => file_contents,
-        Err(e) => return Err(LoadSaveFileError::IoError(e)),
+        Err(e) => return Err(SaveFileError::IoError(e)),
     };
 
     let player_data: PlayerData = match serde_json::from_str(&file_contents) {
         Ok(player_data) => player_data,
-        Err(e) => return Err(LoadSaveFileError::JsonError(e)),
+        Err(e) => return Err(SaveFileError::JsonError(e)),
     };
 
     Ok(player_data)
 }
 
-//            println!("No save file found. Would you like to create one? (y/n)");
-//             if get_bool().unwrap() {
-//                 let player_data = PlayerData::default();
-//                 let player_data_json = serde_json::to_string(&player_data).unwrap();
-//                 fs::write("player_data.json", player_data_json).unwrap();
-//                 return Ok(player_data);
-//             } else {
-//                 return Err(load_save_file_error::FileNotFound);
-//             }
+// saves a PlayerData struct to a file at the specified path
+pub fn save_save_file(path: &str, player_data: &PlayerData) -> Result<(), SaveFileError> {
+
+    let player_data_json = match serde_json::to_string(&player_data) {
+        Ok(player_data_json) => player_data_json,
+        Err(e) => return Err(SaveFileError::JsonError(e)),
+    };
+
+    match fs::write(path, player_data_json) {
+        Ok(_) => Ok(()),
+        Err(e) => Err(SaveFileError::IoError(e)),
+    }
+
+}
